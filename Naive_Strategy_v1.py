f998dd2276febdb448c5c8685a3861abbb0d4c5b
Jacquelyn-You <40428001+Jacquelyn-You@users.noreply.github.com>
2019-12-01 10:52:51 -0500

    Update Naive_Strategy_v1.py

--- Naive_Strategy_v1.py
+++ Naive_Strategy_v1.py
@@ -12,10 +12,10 @@ def Alpha_1(quotes, trades, book, volume = 0.01):
   maybe a dictionary of list
   '''
   
-  buy_price = book['bid'][0]
-  sell_price = book['ask'][0]
-  buy_volume = volume/2
-  sell_volume = volume/2
+  buy_price = [book['bid'][0], book['bid'][1]]
+  sell_price = [book['ask'][0], book['ask'][1]]
+  buy_volume = [volume/4, volume/4]
+  sell_volume = [volume/4, volume/4]
   
   return {'buy_price': buy_price,
           'buy_volume': buy_volume,
