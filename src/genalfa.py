a7d23cc694157360a65514437f423a28f7cd0dc4
Cheng <cy699@nyu.edu>
2019-12-03 22:39:26 -0500

    Add files via upload

--- src/genalfa.py
+++ src/genalfa.py
@@ -1,10 +1,11 @@
-from queue import Queue
 import time 
 from collections import OrderedDict, defaultdict
 from decimal import Decimal
-from utils import ORDER_TYPE, Mode, RECEIVE_TYPE
+from utils import ORDER_TYPE, Mode
+from lightmatchingengine.lightmatchingengine import Side
+
 class Btrade:
-    def __init__(self, mode=Mode.TEST, risk_limit=50, outstanding_limit=180):
+    def __init__(self, init_price, mode=Mode.TEST, start_pos=5, risk_limit=5000000, outstanding_limit=20):
         self.mode = mode
         self.record_manager = defaultdict()
         self.positions = []
@@ -12,6 +13,13 @@ class Btrade:
         self.outstanding_limit = outstanding_limit
         self.pending_orders = defaultdict(list)
         self.price_stock = defaultdict(OrderedDict)
+        self.current_pnl = Decimal()
+        self.current_pos_lst = [[start_pos, init_price]]
+        self.volume = Decimal(0.5)
+        self.target_buy_price = []
+        self.target_sell_price = []
+        self.start_pos = init_price * start_pos
+        self.cost_basis = init_price
         
 
     def data_reveiver(self, data):
@@ -20,16 +28,29 @@ class Btrade:
     def get_singal(self):
         raise NotImplementedError()
 
-    def is_valid_order(self, price):
+    def no_data_reveived(self):
+        return self.target_buy_price and self.target_sell_price
+
+    def is_valid_order(self, side="buy"):
         if self.get_current_risk() > self.risk_limit:
+            return False
+        if side == "sell" and (self.get_current_position() - self.volume) < 0:
             return False
 
         return True
 
+    def remove_all_outstadning_order(self):
+        remove_list = [trade_id for trade_id in self.record_manager]
+        self.pending_orders[ORDER_TYPE.CANCEL].extend(remove_list) 
+
+        self.record_manager = defaultdict()
+        print("remove all orders")
+
+
     def remove_long_standing_order(self):
         remove_list = []
         for trade_id, info in self.record_manager.items():
-            _, create_time, _ = info
+            _, create_time, _ , _ = info
              
             if time.time() - create_time > self.outstanding_limit:
                 self.pending_orders[ORDER_TYPE.CANCEL].append(trade_id)
@@ -38,11 +59,13 @@ class Btrade:
         for trade_id in remove_list:
             del self.record_manager[trade_id]
             print(f"remove order {trade_id}")
-                
+        
+    def get_cost_basis(self, cost, qty):
+        cur_qty = self.get_current_position()
+        self.cost_basis = (self.cost_basis * cur_qty + cost * qty) / (cur_qty + qty)
 
     def receive_fill(self, fill):
-        print(self.price_stock[fill.trade_price])
-        left_size, create_time, cost = self.record_manager[fill.trade_id] 
+        left_size, create_time, cost, side = self.record_manager[fill.trade_id] 
         if left_size > fill.trade_qty:
             left_size -= fill.trade_qty
             qty = fill.trade_qty
@@ -50,34 +73,84 @@ class Btrade:
             qty = left_size
             left_size = 0
 
-        pnl = str((fill.trade_price-cost) * qty)
-        new_pos = {"price": str(fill.trade_price), "qty": str(qty), 
-                    "side": fill.trade_side, "origin_time": create_time,
-                    "cost": str(cost),  "pnl": pnl}
+        new_pos = {"qty": str(qty), "origin_time": create_time,
+                    "cost": str(cost), "origin_side": side,
+                    "realized_pnl": str(self.get_current_pnl()),
+                    "current_pos": str(self.get_current_position())}
+
+        signed_qty = (Decimal(1) if side == Side.BUY else Decimal(-1)) * qty
+
+        if side == Side.SELL:
+            self.current_pnl += qty * (fill.trade_price - self.cost_basis)
+        else:
+            self.get_cost_basis(cost, signed_qty)
+        self.current_pos_lst.append([signed_qty, cost])
+
         self.positions.append(new_pos)
         if left_size > Decimal(0):
-            self.record_manager[fill.trade_id] = (left_size, create_time, cost)
+            self.record_manager[fill.trade_id] = (left_size, create_time, cost, side)
         else:
             del self.record_manager[fill.trade_id]
+
+    def get_current_pnl(self):
+        return self.current_pnl
+
+    def get_current_unr_pnl(self, top_bid, top_ask):
+        res = Decimal() 
+        for qty, cost in self.current_pos_lst:
+            if qty > 0:
+                res += (top_ask - cost) * qty
+            elif qty < 0: 
+                res += (cost - top_bid) * abs(qty)
+
+        return res
+
+    def get_current_position(self):
+        return sum(qty for qty, _ in self.current_pos_lst)
+
 
     def get_current_risk(self):
         res = Decimal() 
         for _, info in self.record_manager.items():
-            left_size, _, cost = info
+            left_size, _, cost, _ = info
             res += cost * left_size
 
         return res
 
+    def process_market_trade(self, trades):
+        self.positions = [] 
+        for trade in trades:
+            if trade.trade_id in self.record_manager:
+                self.receive_fill(trade)
+        
+        return self.positions
+
+    def process(self, order, trades):
+        if order is None:
+            return {} #cross book
+
+        self.positions = []
+        order_id = order.order_id
+        self.record_manager[order_id] = (order.qty, time.time(), order.price, order.side)
+        self.price_stock[order.price][order_id] = order.qty
+
+        for trade in trades:
+            if trade.order_id != order_id:
+                self.receive_fill(trade)
+            
+        return self.positions
+
+
 class Naive1(Btrade):
-    name = "naive3"
+    name = "naive2"
 
     def __init__(self, *args, **kwargs):
         self.target_buy_price = []
         self.target_sell_price = []
-        self.volume = 0.5
+        self.volume = 0.4
         self.order_id = int(time.time()*1000)
         self.data_needs = {"book"}
-        self.waiting_time = 2
+        self.waiting_time = 0.005
         self.pre_timestampe = 0
         self.new_trades = False
         super(Naive1, self).__init__(*args, **kwargs)
@@ -96,20 +169,21 @@ class Naive1(Btrade):
             self.pre_timestampe = timestamp
             self.new_trades = True
 
-    def no_data_reveived(self):
-        return self.target_buy_price and self.target_sell_price
-
     def create_order(self):
        if self.new_trades:
             self.new_trades = False
             for price in self.target_buy_price:
-                self.is_valid_order(price)
+                if not self.is_valid_order():
+                    print("Risk limit reached")
+                    continue
                 self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume , 
                                "side": "buy", "type": "limit",
                                "client_oid": self.get_order_id(),
                                "instrument_id": ""})
             for price in self.target_sell_price:
-                self.is_valid_order(price)
+                if not self.is_valid_order("sell"):
+                    print("Risk limit reached")
+                    continue 
                 self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume , 
                                 "side": "sell", "type": "limit",
                                 "client_oid": self.get_order_id(),
@@ -124,47 +198,23 @@ class Naive1(Btrade):
 
         return self.pending_orders
 
-    def process_market_trade(self, trades):
-        self.positions = [] 
-        for trade in trades:
-            if trade.trade_id in self.record_manager:
-                self.receive_fill(trade)
-        
-        return self.positions
 
-    def process(self, order, trades):
-        if order is None:
-            return {} #cross book
-
-        self.positions = []
-        order_id = order.order_id
-        self.record_manager[order_id] = (order.qty, time.time(), order.price)
-        self.price_stock[order.price][order_id] = order.qty
-
-        for trade in trades:
-            if trade.order_id != order_id:
-                self.receive_fill(trade)
-            
-        return self.positions
-    
-    
-    
-    class Naive2(Btrade):
-    name = "naive_alpha01"
+class Naive2(Btrade):
+    name = "naive_alpha04"
 
     def __init__(self, *args, **kwargs):
         self.target_buy_price = []
         self.target_sell_price = []
         self.weight = []
-        self.volume = 0.5
+        self.volume = 0.2
         self.order_id = int(time.time()*1000)
         self.data_needs = {"book"}
-        self.waiting_time = 2
+        self.waiting_time = 0.1
         self.pre_timestampe = 0
         self.new_trades = False
         self.alpha = 1
         self.threshold = 0.2
-        super(Naive1, self).__init__(*args, **kwargs)
+        super(Naive2, self).__init__(*args, **kwargs)
 
     def get_order_id(self):
         self.order_id += 1
@@ -182,51 +232,60 @@ class Naive1(Btrade):
 
             self.target_buy_price = [bids[0], bids[1], bids[2]]
             self.target_sell_price = [asks[0], asks[1], asks[2]]
-            self.weight = [[0.5, 1.5], [1.5, 0.5]]
+            self.weight = [[Decimal(0.5), Decimal(1.5)], [Decimal(1.5), Decimal(0.5)]]
             self.pre_timestampe = timestamp
             self.new_trades = True
-
-    def no_data_reveived(self):
-        return self.target_buy_price and self.target_sell_price
 
     def create_order(self):
        if self.new_trades:
             self.new_trades = False
             if self.alpha > self.threshold: # go down
-                for (w, price) in zip(self.weight[0], self.target_buy_price):
-                    self.is_valid_order(price)
+                for (w, price) in zip(self.weight[1], self.target_buy_price):
+                    if not self.is_valid_order():
+                        print("Risk limit reached")
+                        continue
                     self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume * w ,
                                    "side": "buy", "type": "limit",
                                    "client_oid": self.get_order_id(),
                                    "instrument_id": ""})
                 for (w, price) in zip(self.weight[1], self.target_sell_price):
-                    self.is_valid_order(price)
+                    if not self.is_valid_order("sell"):
+                        print("Risk limit reached")
+                        continue
                     self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume * w,
                                     "side": "sell", "type": "limit",
                                     "client_oid": self.get_order_id(),
                                     "instrument_id": ""})
             elif self.alpha < - self.threshold:
                 for (w, price) in zip(self.weight[1], self.target_buy_price):
-                    self.is_valid_order(price)
+                    if not self.is_valid_order():
+                        print("Risk limit reached")
+                        continue
                     self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume * w,
                                    "side": "buy", "type": "limit",
                                    "client_oid": self.get_order_id(),
                                    "instrument_id": ""})
                 for (w, price) in zip(self.weight[1], self.target_sell_price):
-                    self.is_valid_order(price)
+                    if not self.is_valid_order("sell"):
+                        print("Risk limit reached")
+                        continue
                     self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume * w,
                                     "side": "sell", "type": "limit",
                                     "client_oid": self.get_order_id(),
                                     "instrument_id": ""})
             else:
                 for price in self.target_buy_price:
-                    self.is_valid_order(price)
+                    if not self.is_valid_order():
+                        print("Risk limit reached")
+                        continue
                     self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume ,
                                    "side": "buy", "type": "limit",
                                    "client_oid": self.get_order_id(),
                                    "instrument_id": ""})
                 for price in self.target_sell_price:
-                    self.is_valid_order(price)
+                    if not self.is_valid_order("sell"):
+                        print("Risk limit reached")
+                        continue
                     self.pending_orders[ORDER_TYPE.ORDER].append({"price": price, "size": self.volume ,
                                     "side": "sell", "type": "limit",
                                     "client_oid": self.get_order_id(),
@@ -241,26 +300,3 @@ class Naive1(Btrade):
         self.create_order()
 
         return self.pending_orders
-
-    def process_market_trade(self, trades):
-        self.positions = [] 
-        for trade in trades:
-            if trade.trade_id in self.record_manager:
-                self.receive_fill(trade)
-        
-        return self.positions
-
-    def process(self, order, trades):
-        if order is None:
-            return {} #cross book
-
-        self.positions = []
-        order_id = order.order_id
-        self.record_manager[order_id] = (order.qty, time.time(), order.price)
-        self.price_stock[order.price][order_id] = order.qty
-
-        for trade in trades:
-            if trade.order_id != order_id:
-                self.receive_fill(trade)
-            
-        return self.positions
--- src/start_test.py
+++ src/start_test.py
@@ -1,7 +1,6 @@
 import time
 import logging
 import datetime
-import configparser
 import trio
 import urllib
 from importlib import import_module
@@ -12,16 +11,17 @@ from matchingengine.engine import MatchEngine
 from marketdata.datadb import DataDB
 from utils import Mode, ORDER_TYPE
 from pymongo import MongoClient
-from genalfa import Naive1
+from genalfa import Naive1, Naive2
 import threading
 from queue import Queue
+import re
 
 MESSAGE_QUEUE = Queue()
 
 class MaketData(threading.Thread):
     def __init__(self, db_reader=None, group=None, target=None, name=None,
-                 args=(), kwargs=None, verbose=None):
-        super(MaketData,self).__init__()
+                 args=(), kwargs=None):
+        super(MaketData, self).__init__()
         self.target = target
         self.name = name
         self.db_reader = db_reader
@@ -29,22 +29,25 @@ class MaketData(threading.Thread):
         self.booked_trade = set()
 
     def run(self):
-        for trade in self.db_reader.db_obj["trades.posts"].find():
+        for trade in self.db_reader.db_obj["trade.post"].find():
             trade_id = trade["trade_id"]
             if trade_id not in self.booked_trade:
-                dt = trade["time"]
-                dt = datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ')
+                rest_flag = False
+                dt_str = trade["time"].strip()
+                if not dt_str:
+                    continue
+                dt = datetime.datetime.strptime(dt_str, '%Y-%m-%dT%H:%M:%S.%fZ')
                 timestamp = time.mktime(dt.timetuple())
-                #print("market: ", trade["price"], trade["side"],  trade["size"], trade_id)
                 if self.pre_time > 0:
+                    rest_flag = (timestamp - self.pre_time) > 60
                     wait_time = max(0, timestamp - self.pre_time)
                     wait_time = min(1, wait_time/100)
                     time.sleep(wait_time)
-                MESSAGE_QUEUE.put(trade)
+                MESSAGE_QUEUE.put([trade, dt_str, rest_flag])
                 self.pre_time = timestamp
                 self.booked_trade.add(trade_id)
 
-        MESSAGE_QUEUE.put("done")
+        MESSAGE_QUEUE.put(["done", "", ""])
         print("done")
         return
 
@@ -52,50 +55,70 @@ class Alfa(threading.Thread):
     def __init__(self, matching_engine=None,  stategy=None,
                  db_writer = None,
                  group=None, target=None, name=None,
-                 args=(), kwargs=None, verbose=None):
+                 args=(), kwargs=None):
         super(Alfa,self).__init__()
         self.target = target
         self.name = name
         self.matching_engine = matching_engine
         self.stategy = stategy
         self.db_writer = db_writer
+        self.retry = 5
         return
 
+    def process_book_reset(self, dt):
+        print("reset book")
+        book_rec = None
+        regex = dt.split(".")[0][:-4]
+        for _ in range(self.retry):
+            if book_rec:
+                break
+            condition = {"timestamp": {"$regex": regex}}
+            book_rec = self.db_writer.regex_find(condition, "book.post")
+            regex = regex[:-1]
+
+
+        self.matching_engine.init_book(book_rec, "ETH-USDT")
+        self.stategy.remove_all_outstadning_order()
+        for _, quotes in self.stategy.creat_requet().items():
+            self.matching_engine.cancel_order(quotes)
+                    
     def run(self):
         while True:
             if not MESSAGE_QUEUE.empty():
-                trade = MESSAGE_QUEUE.get()
+                trade, dt, rest_flag = MESSAGE_QUEUE.get()
                 if trade == "done":
                     break
+                if rest_flag:
+                    self.process_book_reset(dt)
+
                 order, trades = self.matching_engine.add_one_order(trade)
                 position = self.stategy.process_market_trade(trades)
                 if position:
-                    print(f"got something\n {position}")
+                    top_bid = self.matching_engine.get_max_bid()
+                    top_ask = self.matching_engine.get_max_ask()
+                    print(f"Unrealized pnl: {self.stategy.get_current_unr_pnl(top_bid, top_ask)}  pnl: {self.stategy.get_current_pnl()}")
                     self.db_writer.write_data(self.stategy.name, position)
+
                 self.stategy.data_reveiver(self.matching_engine.get_book(), time.time())
                 for exec_type, quotes in self.stategy.creat_requet().items():
                     if exec_type == ORDER_TYPE.ORDER:
                         for order, trades in self.matching_engine.add_batch_order(quotes):
-                            position = self.stategy.process( order, trades)
-                            #if order:
-                                #print("our: ", order.price, order.side)
-                            # if position:
-                            #     print(f"got something\n {position}")
-                            #     self.db_writer.write_data(self.stategy.name, position)
+                            position = self.stategy.process(order, trades)
                     else: 
                          self.matching_engine.cancel_order(quotes)
         return
 
 
 def setup_matching_engine(data_db):
-    book_rec = data_db.get_first_data("book.posts")
+    book_rec = data_db.get_first_data("book.post")
     mathing_engnine = MatchEngine()
     mathing_engnine.init_book(book_rec, "ETH-USDT")
 
     return mathing_engnine
 
-def setup_alfa_engine():
-    alfa = Naive1()
+def setup_alfa_engine(mathing_engnine):
+    init_price = mathing_engnine.get_max_ask()
+    alfa = Naive2(init_price=init_price)
     return alfa
 
 def run():
@@ -103,7 +126,7 @@ def run():
     data_db.start_client()
 
     mathing_engnine = setup_matching_engine(data_db)
-    stategy = setup_alfa_engine()
+    stategy = setup_alfa_engine(mathing_engnine)
 
     p = MaketData(name='producer',db_reader=data_db )
     c = Alfa(name='consumer', matching_engine=mathing_engnine, stategy=stategy, db_writer=data_db)
